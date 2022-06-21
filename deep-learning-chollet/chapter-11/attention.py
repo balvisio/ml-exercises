9068c628941742b1c7878c137ca0d8fb56df6948
Bruno Alvisio <bruno.alvisio@gmail.com>
2022-06-21 15:22:38 +0100

    Annotation and add more flexibility to the TransformerEncoder

--- deep-learning-chollet/chapter-11/attention.py
+++ deep-learning-chollet/chapter-11/attention.py
@@ -1,3 +1,10 @@
+"""
+The following code is based on the the book "Deep Learning with Python" by Francois Chollet (2nd
+edition), Chapter 11, Section 4.
+
+The code has been annotatated and modified to be more clear and more flexible.
+"""
+
 import os, pathlib, shutil, random
 
 import tensorflow as tf
@@ -6,17 +13,35 @@ from tensorflow.keras import layers
 
 
 class TransformerEncoder(layers.Layer):
-    def __init__(self, embed_dim, dense_dim, num_heads, **kwargs) -> None:
+    def __init__(
+        self,
+        head_size_dim,
+        dense_dim,
+        num_heads,
+        value_dim,
+        key_value_seq_length=None,
+        key_embedding_size=None,
+        value_embedding_size=None,
+        attn_output_shape=None,
+        **kwargs,
+    ) -> None:
         super().__init__(**kwargs)
-        self.embed_dim = embed_dim
+        self.head_size_dim = head_size_dim
         self.dense_dim = dense_dim
         self.num_heads = num_heads
+        self.key_value_seq_length = key_value_seq_length
+        self.key_embedding_size = key_embedding_size
+        self.value_embedding_size = value_embedding_size
+        self.attn_output_shape = attn_output_shape
         self.attention = layers.MultiHeadAttention(
-            num_heads=num_heads, key_dim=embed_dim
+            num_heads=num_heads,
+            key_dim=head_size_dim,
+            value_dim=value_dim,
+            output_shape=attn_output_shape,
         )
         self.dense_proj = keras.Sequential(
             [layers.Dense(dense_dim, activation="relu"),
-             layers.Dense(embed_dim),
+             layers.Dense(attn_output_shape),
             ]
         )
         self.layernorm_1 = layers.LayerNormalization()
@@ -25,8 +50,21 @@ class TransformerEncoder(layers.Layer):
     def call(self, inputs, mask=None):
         if mask is not None:
             mask = mask[:, tf.newaxis, :]
+
+        key = inputs
+        value = inputs
+        if self.key_value_seq_length:
+            key = inputs[:, 0:self.key_value_seq_length, :]
+            value = inputs[:, 0:self.key_value_seq_length, :]
+
+        if self.key_embedding_size:
+            key = key[:, :, 0:self.key_embedding_size]
+
+        if self.value_embedding_size:
+            value = value[:, :, 0:self.value_embedding_size]
+
         attention_output = self.attention(
-            inputs, inputs, attention_mask=mask
+            inputs, value, key=key, attention_mask=mask
         )
         proj_input = self.layernorm_1(inputs + attention_output)
         proj_output = self.dense_proj(proj_input)
@@ -35,27 +73,35 @@ class TransformerEncoder(layers.Layer):
     def get_config(self):
         config = super().get_config()
         config.update({
-            "embed_dim": self.embed_dim,
+            "head_size_dim": self.head_size_dim,
             "num_heads": self.num_heads,
             "dense_dim": self.dense_dim,
+            "key_value_seq_length": self.key_value_seq_length,
+            "key_embedding_size": self.key_embedding_size,
+            "value_embeddig_size": self.value_embedding_size,
+            "attn_output_shape": self.attn_output_shape,
         })
         return config
 
 
 def create_ds():
-    base_dir = pathlib.Path("/Users/balvisio/aclImdb")
+    base_dir = pathlib.Path.home() / pathlib.Path("aclImdb")
     val_dir = base_dir / "val"
     train_dir = base_dir / "train"
-    for category in ("neg", "pos"):
-        shutil.rmtree(val_dir / category)
-        os.makedirs(val_dir / category)
-        files = os.listdir(train_dir / category)
-        random.Random(1337).shuffle(files)
-        num_val_samples = int(0.2 * len(files))
-        val_files = files[-num_val_samples:]
-        for fname in val_files:
-            shutil.move(train_dir / category / fname,
-                        val_dir / category / fname)
+
+    if not os.path.isdir(val_dir):
+        print("Building validation directory")
+        for category in ("neg", "pos"):
+            os.makedirs(val_dir / category)
+            files = os.listdir(train_dir / category)
+            random.Random(1337).shuffle(files)
+            num_val_samples = int(0.2 * len(files))
+            val_files = files[-num_val_samples:]
+            for fname in val_files:
+                shutil.move(train_dir / category / fname,
+                            val_dir / category / fname)
+    else:
+        print("Skipping building validation dir")
 
     batch_size = 32
     
@@ -75,18 +121,27 @@ def create_ds():
 
 
 def create_int_ds(train_ds, val_ds, test_ds):
-    max_length = 600
+    max_sequence_length = 789
     max_tokens = 20000
     text_vectorization = layers.TextVectorization(
         max_tokens=max_tokens,
         output_mode="int",
-        output_sequence_length=max_length,
+        output_sequence_length=max_sequence_length,
     )
     
     text_only_train_ds = train_ds.map(lambda x, y: x)
+    """
+    During adapt(), the layer will build a vocabulary of all string tokens seen in the dataset,
+    sorted by occurance count, with ties broken by sort order of the tokens (high to low).
+    At the end of adapt(), if max_tokens is set, the vocabulary wil be truncated to max_tokens size.
+    """
     text_vectorization.adapt(text_only_train_ds)
     int_train_ds = train_ds.map(
         lambda x, y: (text_vectorization(x), y), num_parallel_calls=4)
+    """
+    int_train_ds: It is a tuple of shape:
+    (<tf.Tensor: shape=(batch_size, max_sequence_length), dtype=int64>, <<tf.Tensor: shape=(batch_size,), dtype=int32>)
+    """
     int_val_ds = val_ds.map(
         lambda x, y: (text_vectorization(x), y), num_parallel_calls=4)
     int_test_ds = test_ds.map(
@@ -99,13 +154,57 @@ int_train_ds, int_val_ds, int_test_ds = create_int_ds(train_ds, val_ds, test_ds)
 
 
 vocab_size = 20000
-embed_dim = 256
+
+"""
+Originally called "embed_dim", the "head_size_dim" translates to the "key_dim" of the
+"MultiHeadAttention" and it represents the size of each attention head for query and key
+"""
+head_size_dim = 97
 num_heads = 2
+
+"""
+"word_embed_dim" is the size of the embedding for the Embedding layer.
+"output_shape" defines the size the output dimension of the "MultiHeadAttention" layer. Technically,
+this parameter doesn't need to be equal to "word_embed_dim" but if not, the "TransformerEncoder"
+will give an error when it tries to do the layer 1 normalization
+"""
+word_embed_dim = 456
+output_shape = 456
+
+"""
+This parameter is for the dimension of the Dense layers that come "after" and outside
+the "MultiHeadAttention" layer
+"""
 dense_dim = 32
+value_dim = 188 # This is the size of the head of Value (V). Defaults to "key_dim/embed_dim"
+use_mask = False
+
+"""
+This optional parameter to the encoder is to showcase that the sequence length of the query
+doesn't necessarily need to be equal to the query sequence length.
+"""
+key_value_seq_length = 342
+
+"""
+These optional parameters to the encoder are to showcase that technically the embedding dimensions
+of the K, Q, V can be all different. The matrices multiplications work because remember that the
+K, Q, V input matrices go through the inner dense blocks before the attention is computed.
+"""
+key_embedding_size = 131
+value_embedding_size = 132
 
 inputs = keras.Input(shape=(None,), dtype="int64")
-x = layers.Embedding(vocab_size, embed_dim)(inputs)
-x = TransformerEncoder(embed_dim, dense_dim, num_heads)(x)
+x = layers.Embedding(vocab_size, word_embed_dim, mask_zero=use_mask)(inputs)
+x = TransformerEncoder(
+    head_size_dim,
+    dense_dim,
+    num_heads,
+    value_dim,
+    key_value_seq_length,
+    key_embedding_size,
+    value_embedding_size,
+    output_shape,
+)(x)
 x = layers.GlobalMaxPooling1D()(x)
 x = layers.Dropout(0.5)(x)
 outputs = layers.Dense(1, activation="sigmoid")(x)
@@ -114,6 +213,7 @@ model.compile(
     optimizer="rmsprop",
     loss="binary_crossentropy",
     metrics=["accuracy"],
+    run_eagerly=True,
     )
 model.summary()
 
@@ -136,3 +236,4 @@ model = keras.models.load_model(
     custom_objects={"TransformerEncoder": TransformerEncoder})
 
 print(f"Test acc: {model.evaluate(int_test_ds)[1]:.3f}")
+
